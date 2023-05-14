027a40945b70c9477992f394cfaa4349e867a7de
FlorianPfaff <FlorianPfaff@users.noreply.github.com>
2023-05-14 13:26:08 +0000

    [MegaLinter] Apply linters fixes

--- pyrecest/distributions/abstract_bounded_nonperiodic_distribution.py
+++ pyrecest/distributions/abstract_bounded_nonperiodic_distribution.py
@@ -1,5 +1,6 @@
 from .abstract_bounded_domain_distribution import AbstractBoundedDomainDistribution
 
+
 class AbstractBoundedNonPeriodicDistribution(AbstractBoundedDomainDistribution):
     def mean(self):
         raise ValueError("Mean currently not supported.")
