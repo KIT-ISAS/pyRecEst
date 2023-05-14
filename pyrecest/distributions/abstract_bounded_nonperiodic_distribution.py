25f2e3f9b46160adb13d06eee78adfe0d2afedfe
Florian Pfaff <pfaff@kit.edu>
2023-05-14 15:24:34 +0200

    Added AbstractBoundedNonperiodicDistribution

---
+++ pyrecest/distributions/abstract_bounded_nonperiodic_distribution.py
@@ -0,0 +1,5 @@
+from .abstract_bounded_domain_distribution import AbstractBoundedDomainDistribution
+
+class AbstractBoundedNonPeriodicDistribution(AbstractBoundedDomainDistribution):
+    def mean(self):
+        raise ValueError("Mean currently not supported.")
