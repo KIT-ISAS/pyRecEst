47a04ea3455b46563908397ed261521db2d65f68
FlorianPfaff <FlorianPfaff@users.noreply.github.com>
2023-05-15 16:06:48 +0000

    [MegaLinter] Apply linters fixes

--- pyrecest/tests/distributions/test_linear_mixture.py
+++ pyrecest/tests/distributions/test_linear_mixture.py
@@ -1,18 +1,28 @@
 import unittest
-import numpy as np
-from pyrecest.distributions.nonperiodic.linear_mixture import LinearMixture
-from pyrecest.distributions import GaussianDistribution
 from warnings import catch_warnings, simplefilter
+
+import numpy as np
+from pyrecest.distributions import GaussianDistribution
+from pyrecest.distributions.nonperiodic.linear_mixture import LinearMixture
+
 
 class LinearMixtureTest(unittest.TestCase):
     def test_constructor_warning(self):
         with catch_warnings(record=True) as w:
             simplefilter("always")
-            LinearMixture([GaussianDistribution(np.array(1), np.array(1)), GaussianDistribution(np.array(50), np.array(1))], np.array([0.3, 0.7]))
+            LinearMixture(
+                [
+                    GaussianDistribution(np.array(1), np.array(1)),
+                    GaussianDistribution(np.array(50), np.array(1)),
+                ],
+                np.array([0.3, 0.7]),
+            )
             self.assertEqual(len(w), 1)
             self.assertTrue(issubclass(w[-1].category, UserWarning))
-            self.assertIn("For mixtures of Gaussians, consider using GaussianMixture.", str(w[-1].message))
-
+            self.assertIn(
+                "For mixtures of Gaussians, consider using GaussianMixture.",
+                str(w[-1].message),
+            )
 
     def test_pdf(self):
         gm1 = GaussianDistribution(np.array([1, 1]), np.diag([2, 3]))
@@ -25,7 +35,10 @@ class LinearMixtureTest(unittest.TestCase):
         x, y = np.meshgrid(np.linspace(-2, 2, 100), np.linspace(-2, 2, 100))
         points = np.column_stack((x.ravel(), y.ravel()))
 
-        np.testing.assert_allclose(lm.pdf(points), 0.3 * gm1.pdf(points) + 0.7 * gm2.pdf(points), atol=1e-20)
+        np.testing.assert_allclose(
+            lm.pdf(points), 0.3 * gm1.pdf(points) + 0.7 * gm2.pdf(points), atol=1e-20
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
