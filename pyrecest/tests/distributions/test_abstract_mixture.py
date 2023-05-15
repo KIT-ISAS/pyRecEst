c258d35ac91552cef357c8aab66a9cfb29ab8031
Florian Pfaff <pfaff@kit.edu>
2023-05-15 15:27:03 +0200

    Reduced redundancy in a test case

--- pyrecest/tests/distributions/test_abstract_mixture.py
+++ pyrecest/tests/distributions/test_abstract_mixture.py
@@ -8,40 +8,29 @@ from pyrecest.distributions.hypersphere_subset.custom_hyperhemispherical_distrib
 
 class AbstractMixtureTest(unittest.TestCase):
 
+    def _test_sample(self, mix, n):
+        for sampling_method in [mix.sample_metropolis_hastings, mix.sample]:
+            s = sampling_method(n)
+            self.assertEqual(s.shape, (n, mix.input_dim))
+        return s
+
     def test_sample_metropolis_hastings_basics_only_t2(self):
         vmf = ToroidalWrappedNormalDistribution(np.array([1, 0]), np.eye(2))
         mix = HypertoroidalMixture([vmf, vmf.shift(np.array([1, 1]))], [0.5, 0.5])
-        n = 10
-        s = mix.sample_metropolis_hastings(n)
-        self.assertEqual(s.shape, (n, mix.dim))
-
-        s2 = mix.sample(n)
-        self.assertEqual(s2.shape, (n, mix.dim))
+        self._test_sample(mix, 10)
 
     def test_sample_metropolis_hastings_basics_only_s2(self):
         vmf1 = VonMisesFisherDistribution(np.array([1, 0, 0]), 2)
         vmf2 = VonMisesFisherDistribution(np.array([0, 1, 0]), 2)
         mix = HypersphericalMixture([vmf1, vmf2], [0.5, 0.5])
-        n = 10
-        s = mix.sample_metropolis_hastings(n)
-        self.assertEqual(s.shape, (n, mix.input_dim))
-        self.assertTrue(np.allclose(np.linalg.norm(s, axis=1), np.ones(n), rtol=1e-10))
-
-        s2 = mix.sample(n)
-        self.assertEqual(s2.shape, (n, mix.input_dim))
-        self.assertTrue(np.allclose(np.linalg.norm(s, axis=1), np.ones(n), rtol=1e-10))
+        s = self._test_sample(mix, 10)
+        self.assertTrue(np.allclose(np.linalg.norm(s, axis=1), np.ones(10), rtol=1e-10))
 
     def test_sample_metropolis_hastings_basics_only_h2(self):
         vmf = VonMisesFisherDistribution(np.array([1, 0, 0]), 2)
         mix = CustomHyperhemisphericalDistribution(lambda x: vmf.pdf(x) + vmf.pdf(-x), 2)
-        n = 10
-        s = mix.sample_metropolis_hastings(n)
-        self.assertEqual(s.shape, (n, mix.input_dim))
-        self.assertTrue(np.allclose(np.linalg.norm(s, axis=1), np.ones(n), rtol=1e-10))
-
-        s2 = mix.sample(n)
-        self.assertEqual(s2.shape, (n, mix.input_dim))
-        self.assertTrue(np.allclose(np.linalg.norm(s, axis=1), np.ones(n), rtol=1e-10))
+        s = self._test_sample(mix, 10)
+        self.assertTrue(np.allclose(np.linalg.norm(s, axis=1), np.ones(10), rtol=1e-10))
 
 if __name__ == '__main__':
     unittest.main()
