4224f808c51db0ba9c71af0be91be79d567fffa6
Florian Pfaff <pfaff@kit.edu>
2023-06-12 05:46:03 +0200

    Now all coordinate transformations available for spherical coordinates

--- pyrecest/tests/distributions/test_abstract_hyperspherical_distribution.py
+++ pyrecest/tests/distributions/test_abstract_hyperspherical_distribution.py
@@ -54,14 +54,13 @@ class AbstractHypersphericalDistributionTest(unittest.TestCase):
         self.assertLess(np.linalg.norm(vmf.mean_direction_numerical() - mu), 1e-6)
 
     def test_plotting_error_free_2d(self):
-        """Tests the numerical mode calculation."""
+        """Tests the plotting function"""
 
         mu = np.array([1, 1, 2])
         mu = mu / np.linalg.norm(mu)
         kappa = 10
         vmf = VonMisesFisherDistribution(mu, kappa)
         vmf.plot()
-        plt.close()
 
 
 if __name__ == "__main__":
