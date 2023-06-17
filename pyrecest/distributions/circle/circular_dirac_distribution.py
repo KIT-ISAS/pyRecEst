4fc6183bb2d2e77a94e0bf1a8157ec92c9038040
Florian Pfaff <pfaff@kit.edu>
2023-06-17 06:46:54 +0200

    Removed __init__ for CircularDiracDist, use inheritance

--- pyrecest/distributions/circle/circular_dirac_distribution.py
+++ pyrecest/distributions/circle/circular_dirac_distribution.py
@@ -8,20 +8,9 @@ from .abstract_circular_distribution import AbstractCircularDistribution
 class CircularDiracDistribution(
     HypertoroidalDiracDistribution, AbstractCircularDistribution
 ):
-    @beartype
-    def __init__(self, d: np.ndarray, w: np.ndarray | None = None):
-        """
-        Initializes a CircularDiracDistribution instance.
-
-        Args:
-            d (np.ndarray): The Dirac locations.
-            w (Optional[np.ndarray]): The weights for each Dirac location.
-        """
-        super().__init__(d, w, dim=1)
-        assert self.d.shape[0] == self.w.shape[0], "The shapes of d and w should match."
 
     def plot_interpolated(self, _):
         """
-        Raises an exception since interpolation is not available for WDDistribution.
+        Raises an exception since interpolation is not available for CircularDiracDistribution.
         """
-        raise NotImplementedError("No interpolation available for WDDistribution.")
+        raise NotImplementedError("No interpolation available for CircularDiracDistribution.")
