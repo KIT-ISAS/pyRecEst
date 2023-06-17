50dc203ec69c60eea00dbd32eabf5bb583eedd5a
FlorianPfaff <FlorianPfaff@users.noreply.github.com>
2023-06-17 04:49:07 +0000

    [MegaLinter] Apply linters fixes

--- pyrecest/distributions/circle/circular_dirac_distribution.py
+++ pyrecest/distributions/circle/circular_dirac_distribution.py
@@ -1,5 +1,3 @@
-import numpy as np
-from beartype import beartype
 
 from ..hypertorus.hypertoroidal_dirac_distribution import HypertoroidalDiracDistribution
 from .abstract_circular_distribution import AbstractCircularDistribution
@@ -8,9 +6,10 @@ from .abstract_circular_distribution import AbstractCircularDistribution
 class CircularDiracDistribution(
     HypertoroidalDiracDistribution, AbstractCircularDistribution
 ):
-
     def plot_interpolated(self, _):
         """
         Raises an exception since interpolation is not available for CircularDiracDistribution.
         """
-        raise NotImplementedError("No interpolation available for CircularDiracDistribution.")
+        raise NotImplementedError(
+            "No interpolation available for CircularDiracDistribution."
+        )
