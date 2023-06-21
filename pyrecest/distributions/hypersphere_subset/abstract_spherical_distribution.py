9498ef997504dacd3f4e08c6329f9a705ca33e63
FlorianPfaff <FlorianPfaff@users.noreply.github.com>
2023-06-21 04:44:33 +0000

    [MegaLinter] Apply linters fixes

--- pyrecest/distributions/hypersphere_subset/abstract_spherical_distribution.py
+++ pyrecest/distributions/hypersphere_subset/abstract_spherical_distribution.py
@@ -1,9 +1,10 @@
-from .abstract_hyperspherical_distribution import (
-    AbstractHypersphericalDistribution,
-)
+from .abstract_hyperspherical_distribution import AbstractHypersphericalDistribution
 from .abstract_sphere_subset_distribution import AbstractSphereSubsetDistribution
 
-class AbstractSphericalDistribution(AbstractSphereSubsetDistribution, AbstractHypersphericalDistribution):
+
+class AbstractSphericalDistribution(
+    AbstractSphereSubsetDistribution, AbstractHypersphericalDistribution
+):
     def __init__(self):
         AbstractSphereSubsetDistribution.__init__(self)
         AbstractHypersphericalDistribution.__init__(self, dim=2)
