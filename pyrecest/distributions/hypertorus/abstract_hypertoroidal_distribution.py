3c0ded51ef6c2c3b7a0cbc7ae33fb0e09ce49c41
Florian Pfaff <pfaff@kit.edu>
2023-07-08 08:28:58 +0200

    Now collecting extra plot commands in **kwargs

--- pyrecest/distributions/hypertorus/abstract_hypertoroidal_distribution.py
+++ pyrecest/distributions/hypertorus/abstract_hypertoroidal_distribution.py
@@ -161,14 +161,14 @@ class AbstractHypertoroidalDistribution(AbstractPeriodicDistribution):
         dist = 0.5 * self.integrate_fun_over_domain(total_variation_dist_fun, self.dim)
         return dist
 
-    def plot(self, *args, resolution=None):
+    def plot(self, resolution=None, **kwargs):
         if resolution is None:
             resolution = 128
 
         if self.dim == 1:
             theta = np.linspace(0, 2 * np.pi, resolution)
             f_theta = self.pdf(theta)
-            p = plt.plot(theta, f_theta, *args)
+            p = plt.plot(theta, f_theta, **kwargs)
             AbstractHypertoroidalDistribution.setup_axis_circular("x")
         elif self.dim == 2:
             step = 2 * np.pi / resolution
@@ -177,7 +177,7 @@ class AbstractHypertoroidalDistribution(AbstractPeriodicDistribution):
             )
             f = self.pdf(np.vstack((alpha.ravel(), beta.ravel())))
             f = f.reshape(alpha.shape)
-            p = plt.contourf(alpha, beta, f, *args)
+            p = plt.contourf(alpha, beta, f, **kwargs)
             AbstractHypertoroidalDistribution.setup_axis_circular("x")
             AbstractHypertoroidalDistribution.setup_axis_circular("y")
         elif self.dim == 3:
