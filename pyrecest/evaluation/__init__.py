67ca57ca2b5971c71aa740aba1761dc0346ec84e
Florian Pfaff <pfaff@kit.edu>
2023-09-05 10:17:55 +0200

    Added configure_for_filter for eval for pf and kf

--- pyrecest/evaluation/__init__.py
+++ pyrecest/evaluation/__init__.py
@@ -1,4 +1,5 @@
 from .check_and_fix_params import check_and_fix_params
+from .configure_for_filter import configure_for_filter
 from .generate_groundtruth import generate_groundtruth
 from .generate_measurements import generate_measurements
 from .scenario_database import scenario_database
@@ -8,4 +9,5 @@ __all__ = [
     "generate_measurements",
     "scenario_database",
     "check_and_fix_params",
+    "configure_for_filter",
 ]
--- pyrecest/tests/test_evaluation.py
+++ pyrecest/tests/test_evaluation.py
@@ -1,12 +1,18 @@
 import unittest
 
 import numpy as np
+from pyrecest.distributions import (
+    GaussianDistribution,
+    HypertoroidalWrappedNormalDistribution,
+)
 from pyrecest.evaluation import (
     check_and_fix_params,
+    configure_for_filter,
     generate_groundtruth,
     generate_measurements,
     scenario_database,
 )
+from pyrecest.filters import HypertoroidalParticleFilter, KalmanFilter
 
 
 class TestEvalation(unittest.TestCase):
@@ -34,6 +40,50 @@ class TestEvalation(unittest.TestCase):
                 measurements[i].shape[0],
                 scenario_param["n_meas_at_individual_time_step"][i],
             )
+
+    def test_kf_filter(self):
+        filterParam = {"name": "kf", "parameter": 10}
+        scenarioParam = {
+            "initial_prior": GaussianDistribution(np.array([0, 0]), np.eye(2)),
+            "inputs": None,
+            "manifoldType": "Euclidean",
+        }
+
+        configured_filter, predictionRoutine, *_ = configure_for_filter(
+            filterParam, scenarioParam
+        )
+
+        self.assertIsInstance(configured_filter, KalmanFilter)
+        self.assertIsNotNone(predictionRoutine)
+
+    def test_pf_filter(self):
+        filterParam = {"name": "pf", "parameter": 100}
+        scenarioParam = {
+            "initial_prior": HypertoroidalWrappedNormalDistribution(
+                np.array([0, 0]), np.eye(2)
+            ),
+            "inputs": None,
+            "manifold_type": "hypertorus",
+            "gen_next_state_with_noise": lambda x: x,
+        }
+
+        configured_filter, predictionRoutine, *_ = configure_for_filter(
+            filterParam, scenarioParam
+        )
+
+        self.assertIsInstance(configured_filter, HypertoroidalParticleFilter)
+        self.assertIsNotNone(predictionRoutine)
+
+    def test_unsupported_filter(self):
+        filterParam = {"name": "unsupported_filter", "parameter": 10}
+        scenarioParam = {
+            "initial_prior": "some_initial_prior",
+            "inputs": None,
+            "manifold_type": "Euclidean",
+        }
+
+        with self.assertRaises(ValueError):
+            configure_for_filter(filterParam, scenarioParam)
 
 
 if __name__ == "__main__":
