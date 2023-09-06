f6a4cfc83aacab63ff9e596a05cc367103adf1ce
Florian Pfaff <pfaff@kit.edu>
2023-09-06 16:33:59 +0200

    Eval: Split up function to get distance fun, mean calc and label

--- pyrecest/evaluation/__init__.py
+++ pyrecest/evaluation/__init__.py
@@ -2,11 +2,13 @@ from .check_and_fix_params import check_and_fix_params
 from .configure_for_filter import configure_for_filter
 from .generate_groundtruth import generate_groundtruth
 from .generate_measurements import generate_measurements
-from .get_distance_fun_mean_calc_and_label import get_distance_fun_mean_calc_and_label
 from .iterate_configs_and_runs import iterate_configs_and_runs
 from .perform_predict_update_cycles import perform_predict_update_cycles
 from .scenario_database import scenario_database
 from .start_evaluation import start_evaluation
+from .get_axis_label import get_axis_label
+from .get_distance_function import get_distance_function
+from .get_extract_mean import get_extract_mean
 
 __all__ = [
     "generate_groundtruth",
@@ -16,6 +18,8 @@ __all__ = [
     "configure_for_filter",
     "perform_predict_update_cycles",
     "iterate_configs_and_runs",
-    "get_distance_fun_mean_calc_and_label",
     "start_evaluation",
+    "get_axis_label",
+    "get_distance_function",
+    "get_extract_mean",
 ]
