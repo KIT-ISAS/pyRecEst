cf48e309a9158671f50d65c70ae8a01d1c243a70
FlorianPfaff <FlorianPfaff@users.noreply.github.com>
2023-09-06 14:54:09 +0000

    [MegaLinter] Apply linters fixes

--- pyrecest/evaluation/__init__.py
+++ pyrecest/evaluation/__init__.py
@@ -2,13 +2,13 @@ from .check_and_fix_params import check_and_fix_params
 from .configure_for_filter import configure_for_filter
 from .generate_groundtruth import generate_groundtruth
 from .generate_measurements import generate_measurements
+from .get_axis_label import get_axis_label
+from .get_distance_function import get_distance_function
+from .get_extract_mean import get_extract_mean
 from .iterate_configs_and_runs import iterate_configs_and_runs
 from .perform_predict_update_cycles import perform_predict_update_cycles
 from .scenario_database import scenario_database
 from .start_evaluation import start_evaluation
-from .get_axis_label import get_axis_label
-from .get_distance_function import get_distance_function
-from .get_extract_mean import get_extract_mean
 
 __all__ = [
     "generate_groundtruth",
--- pyrecest/evaluation/get_distance_function.py
+++ pyrecest/evaluation/get_distance_function.py
@@ -3,51 +3,63 @@ from numpy.linalg import norm
 from pyrecest.distributions import AbstractHypertoroidalDistribution
 
 
-def get_distance_function(mode, additional_params=None, nSymm=None, symmetryOffsets=None):
+def get_distance_function(
+    mode, additional_params=None, nSymm=None, symmetryOffsets=None
+):
     if nSymm is not None or symmetryOffsets is not None or "Symm" in mode:
         raise NotImplementedError("Not implemented yet")
 
     if "circle" in mode or "hypertorus" in mode:
+
         def distance_function(xest, xtrue):
             return norm(AbstractHypertoroidalDistribution.angular_error(xest, xtrue))
 
     elif "hypersphere" in mode:
+
         def distance_function(x1, x2):
             return np.arccos(np.dot(x1, x2))
 
     elif "hypersphereSymmetric" in mode:
+
         def distance_function(x1, x2):
             return min(np.arccos(np.dot(x1, x2)), np.arccos(np.dot(x1, -x2)))
 
     elif "se2" in mode or "se2linear" in mode:
+
         def distance_function(x1, x2):
             return norm(x1[1:3, :] - x2[1:3, :])
 
     elif "se2bounded" in mode:
+
         def distance_function(xest, xtrue):
-            return norm(AbstractHypertoroidalDistribution.angular_error(xest[0, :], xtrue[0, :]))
+            return norm(
+                AbstractHypertoroidalDistribution.angular_error(xest[0, :], xtrue[0, :])
+            )
 
     elif "se3" in mode or "se3linear" in mode:
+
         def distance_function(x1, x2):
             return norm(x1[4:7, :] - x2[4:7, :])
 
     elif "se3bounded" in mode:
+
         def distance_function(x1, x2):
             return min(
-                np.arccos(np.dot(x1[:4], x2[:4])),
-                np.arccos(np.dot(x1[:4], -x2[:4]))
+                np.arccos(np.dot(x1[:4], x2[:4])), np.arccos(np.dot(x1[:4], -x2[:4]))
             )
 
     elif "euclidean" in mode and "MTT" not in mode:
+
         def distance_function(x1, x2):
             return norm(x1 - x2)
 
     elif "MTTEuclidean" in mode:
         additional_params.get("cutoff_distance", 1000000)
+
         def distance_function(x1, x2):
             raise NotImplementedError("Not implemented yet")
 
     else:
         raise ValueError("Mode not recognized")
-    
+
     return distance_function
