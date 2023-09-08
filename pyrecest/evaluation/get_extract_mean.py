5150b028c712a0aafc2abd789cfb969e6504b5b4
Florian Pfaff <pfaff@kit.edu>
2023-09-08 15:04:52 +0200

    Renamed variable mode in eval functions

--- pyrecest/evaluation/get_extract_mean.py
+++ pyrecest/evaluation/get_extract_mean.py
@@ -1,42 +1,42 @@
-def get_extract_mean(mode):
-    if "symm" in mode:
+def get_extract_mean(manifold_name):
+    if "symm" in manifold_name:
         raise NotImplementedError("Not implemented yet")
 
-    if "circle" in mode or "hypertorus" in mode:
+    if "circle" in manifold_name or "hypertorus" in manifold_name:
 
         def extract_mean(filter_state):
             return filter_state.mean_direction()
 
-    elif "hypersphere" in mode:
+    elif "hypersphere" in manifold_name:
 
         def extract_mean(filter_state):
             return filter_state.mean_direction()  # Stub
 
-    elif "hypersphereSymmetric" in mode:
+    elif "hypersphereSymmetric" in manifold_name:
         extract_mean = "custom"
 
-    elif "se2" in mode or "se2linear" in mode:
+    elif "se2" in manifold_name or "se2linear" in manifold_name:
         raise NotImplementedError("Not implemented yet")
 
-    elif "se2bounded" in mode:
+    elif "se2bounded" in manifold_name:
         raise NotImplementedError("Not implemented yet")
 
-    elif "se3" in mode or "se3linear" in mode:
+    elif "se3" in manifold_name or "se3linear" in manifold_name:
 
         def extract_mean(filter_state):
             return filter_state.hybrid_mean()
 
-    elif "se3bounded" in mode:
+    elif "se3bounded" in manifold_name:
 
         def extract_mean(filter_state):
             return filter_state.hybrid_mean()
 
-    elif "euclidean" in mode and "MTT" not in mode:
+    elif "euclidean" in manifold_name and "MTT" not in manifold_name:
 
         def extract_mean(filter_state):
             return filter_state.mean()
 
-    elif "MTTEuclidean" in mode:
+    elif "MTTEuclidean" in manifold_name:
 
         def extract_mean(_):
             raise NotImplementedError("Not implemented yet")
