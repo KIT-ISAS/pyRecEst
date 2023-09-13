39f47cebd15ca31853be6a3420cad6f5351b82d7
Florian Pfaff <pfaff@kit.edu>
2023-09-13 23:53:35 +0200

    Added HemisphericalUniformDistribution

--- pyrecest/tests/distributions/test_hyperhemispherical_uniform_distribution.py
+++ pyrecest/tests/distributions/test_hyperhemispherical_uniform_distribution.py
@@ -4,6 +4,12 @@ import unittest
 import numpy as np
 from pyrecest.distributions import HyperhemisphericalUniformDistribution
 
+def get_random_points(n, d):
+    np.random.seed(10)
+    points = np.random.randn(n, d + 1)
+    points = points[points[:, -1] >= 0, :]
+    points /= np.reshape(np.linalg.norm(points, axis=1), (-1, 1))
+    return points
 
 class TestHyperhemisphericalUniformDistribution(unittest.TestCase):
     """Test for uniform distribution for hyperhemispheres"""
@@ -11,28 +17,22 @@ class TestHyperhemisphericalUniformDistribution(unittest.TestCase):
     def test_pdf_2d(self):
         hhud = HyperhemisphericalUniformDistribution(2)
 
-        np.random.seed(10)
-        points = np.random.randn(3, 100)
-        points = points[:, points[2, :] >= 0]
-        points /= np.linalg.norm(points, axis=0)
+        points = get_random_points(100, 2)
 
         self.assertTrue(
             np.allclose(
-                hhud.pdf(points), np.ones(points.shape[1]) / (2 * np.pi), atol=1e-6
+                hhud.pdf(points), np.ones(points.shape[0]) / (2 * np.pi), atol=1e-6
             )
         )
 
     def test_pdf_3d(self):
         hhud = HyperhemisphericalUniformDistribution(3)
 
-        np.random.seed(10)
-        points = np.random.randn(4, 100)
-        points = points[:, points[3, :] >= 0]
-        points /= np.linalg.norm(points, axis=0)
+        points = get_random_points(100, 3)
 
         self.assertTrue(
             np.allclose(
-                hhud.pdf(points), np.ones(points.shape[1]) / (np.pi**2), atol=1e-6
+                hhud.pdf(points), np.ones(points.shape[0]) / (np.pi**2), atol=1e-6
             )
         )
 
