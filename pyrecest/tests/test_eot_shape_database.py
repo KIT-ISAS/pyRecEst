f5432f2255f10ecdb5cf9c9dd3150a155927402e
Florian Pfaff <pfaff@kit.edu>
2023-09-13 08:40:22 +0200

    Added cross object

--- pyrecest/tests/test_eot_shape_database.py
+++ pyrecest/tests/test_eot_shape_database.py
@@ -1,10 +1,12 @@
 import unittest
 
-from pyrecest.evaluation.eot_shape_database import Star, StarShapedPolygon
+from pyrecest.evaluation.eot_shape_database import Star, StarShapedPolygon, Cross
 from shapely.geometry import Polygon
+from shapely.plotting import plot_polygon
+import matplotlib.pyplot as plt
 
 
-class TestStarConvexPolygon(unittest.TestCase):
+class TestStarSahpedPolygon(unittest.TestCase):
     def setUp(self):
         self.square_star_convex_poly = StarShapedPolygon(
             [(-0.5, -0.5), (-0.5, 0.5), (0.5, 0.5), (0.5, -0.5)]
@@ -23,12 +25,30 @@ class TestStarConvexPolygon(unittest.TestCase):
             self.square_star_convex_poly.area,
         )
 
+
+class TestStar(unittest.TestCase):
     def test_compute_kernel_star_convex(self):
         star_full = Star(0.5)
         star_kernel = star_full.compute_kernel()
         # Kernel no more than 50% of the area of the original polygon
         self.assertLess(star_kernel.area, 0.5 * star_full.area)
 
+
+class TestCross(unittest.TestCase):
+    def setUp(self) -> None:
+        self.cross_full = Cross(2, 1, 2, 3)
+        self.cross_kernel = self.cross_full.compute_kernel()
+        
+    def test_area(self):
+        self.assertEqual(self.cross_full.area, 5)
+        
+    def test_compute_kernel_cross_convex(self):
+        # Determining the kernel of a cross-shaped polygon is trivial
+        self.assertEqual(self.cross_kernel.area, 2)
+        
+    def test_plotting(self):
+        plot_polygon(self.cross_full)
+        plot_polygon(self.cross_kernel, color='red')
 
 if __name__ == "__main__":
     unittest.main()
