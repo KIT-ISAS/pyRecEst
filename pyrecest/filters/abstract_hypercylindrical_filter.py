101423ee80de8b86d9b3f3efe0e6425ec1f58309
Florian Pfaff <pfaff@kit.edu>
2023-09-15 15:52:04 +0200

    Added AbstractHypercylindricalFilter

---
+++ pyrecest/filters/abstract_hypercylindrical_filter.py
@@ -0,0 +1,7 @@
+from .abstract_lin_periodic_filter import AbstractLinPeriodicFilter
+
+
+class AbstractHypercylindricalFilter(AbstractLinPeriodicFilter):
+
+    def get_point_estimate(self):
+        return self.filter_state.hybrid_mean()
