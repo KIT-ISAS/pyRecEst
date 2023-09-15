a66124fcd47acc9b5a1317194736511638574b49
FlorianPfaff <FlorianPfaff@users.noreply.github.com>
2023-09-15 13:55:32 +0000

    [MegaLinter] Apply linters fixes

--- pyrecest/filters/abstract_hypercylindrical_filter.py
+++ pyrecest/filters/abstract_hypercylindrical_filter.py
@@ -2,6 +2,5 @@ from .abstract_lin_periodic_filter import AbstractLinPeriodicFilter
 
 
 class AbstractHypercylindricalFilter(AbstractLinPeriodicFilter):
-
     def get_point_estimate(self):
         return self.filter_state.hybrid_mean()
