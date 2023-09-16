c6cdacb420c5f332944ebe6c2eec4c1015b262f8
FlorianPfaff <FlorianPfaff@users.noreply.github.com>
2023-09-16 19:54:19 +0000

    [MegaLinter] Apply linters fixes

--- pyrecest/sampling/abstract_sampler.py
+++ pyrecest/sampling/abstract_sampler.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
+
 import numpy as np
 
-class AbstractSampler(ABC):       
+
+class AbstractSampler(ABC):
     @abstractmethod
     def sample_stochastic(self, n_samples: int, dim: int) -> np.ndarray:
         raise NotImplementedError("Abstract method not implemented!")
