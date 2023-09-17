d3851cc0f1a114317878f4ab35a5d7decb167bb8
Florian Pfaff <pfaff@kit.edu>
2023-09-17 21:38:45 +0200

    Added AbstractHyperhemisphericalFilter

---
+++ pyrecest/filters/abstract_hyperhemispherical_filter.py
@@ -0,0 +1,7 @@
+from .abstract_manifold_specific_filter import AbstractManifoldSpecificFilter
+
+
+class AbstractHyperhemisphericalFilter(AbstractManifoldSpecificFilter):
+    """
+    Abstract class representing a hyperhemispherical filter.
+    """
