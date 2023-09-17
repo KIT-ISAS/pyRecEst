15b04b6c5de0636a7e384928251d01d88bf5920a
Florian Pfaff <pfaff@kit.edu>
2023-09-17 21:50:54 +0200

    Added AbstractPeriodicGridDistribution

---
+++ pyrecest/distributions/abstract_periodic_grid_distribution.py
@@ -0,0 +1,5 @@
+from .abstract_bounded_grid_distribution import AbstractBoundedGridDistribution
+
+
+class AbstractPeriodicGridDistribution(AbstractBoundedGridDistribution):
+    """Class for grid distributions on periodic domains"""
