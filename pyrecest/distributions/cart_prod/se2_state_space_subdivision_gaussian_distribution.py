b9f1b3ba8fb928bf6f2708fc347f2d376e7febae
Florian Pfaff <pfaff@kit.edu>
2023-09-17 21:39:56 +0200

    Added SE2StateSpaceSubdivisionGaussianDistribution

---
+++ pyrecest/distributions/cart_prod/se2_state_space_subdivision_gaussian_distribution.py
@@ -0,0 +1,5 @@
+from pyrecest.distributions.cart_prod.hypercylindrical_state_space_subdivision_gaussian_distribution import HypercylindricalStateSpaceSubdivisionGaussianDistribution
+from pyrecest.distributions.abstract_se2_distribution import AbstractSE2Distribution
+
+class SE2StateSpaceSubdivisionGaussianDistribution(HypercylindricalStateSpaceSubdivisionGaussianDistribution, AbstractSE2Distribution):
+    pass
