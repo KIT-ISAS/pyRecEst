a5da4b2132f0150c70987a495368b84e11f911a7
Florian Pfaff <pfaff@kit.edu>
2023-09-17 11:36:18 +0200

    Added CircularUniformSampler

---
+++ pyrecest/sampling/hypertoroidal_sampler.py
@@ -0,0 +1,28 @@
+from beartype import beartype
+from .abstract_sampler import AbstractSampler
+from pyrecest.distributions import CircularUniformDistribution
+import numpy as np
+
+
+class AbstractHypertoroidalSampler(AbstractSampler):
+    pass
+
+
+class AbstractCircularSampler(AbstractHypertoroidalSampler):
+    pass
+
+
+class CircularUniformSampler(AbstractCircularSampler):
+    @beartype
+    def sample_stochastic(self, n_samples: int):
+        return CircularUniformDistribution().sample(n_samples)
+
+    @beartype
+    def get_grid(self, grid_density_parameter: int) -> np.ndarray:
+        """
+        Returns an equidistant grid of points on the circle [0,2*pi).
+        """
+        points = np.linspace(0, 2 * np.pi, grid_density_parameter, endpoint=False)
+        # Set it to the middle of the interval instead of the start
+        points += (2 * np.pi / grid_density_parameter) / 2
+        return points
