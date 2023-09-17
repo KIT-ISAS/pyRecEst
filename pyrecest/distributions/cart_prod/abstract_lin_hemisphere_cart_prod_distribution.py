feb4e699fc426d17fed8c4de769c4a6bc69df493
Florian Pfaff <pfaff@kit.edu>
2023-09-17 21:54:00 +0200

    Added AbstractLinHemisphereCartProdDistribution

---
+++ pyrecest/distributions/cart_prod/abstract_lin_hemisphere_cart_prod_distribution.py
@@ -0,0 +1,5 @@
+from .abstract_lin_hyperhemisphere_cart_prod_distribution import AbstractLinHyperhemisphereCartProdDistribution
+
+
+class AbstractLinHemisphereCartProdDistribution(AbstractLinHyperhemisphereCartProdDistribution):
+    pass
