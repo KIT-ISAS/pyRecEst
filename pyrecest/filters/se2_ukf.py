ae044077393c53d456e15bb714438132d0cdc6bc
FlorianPfaff <FlorianPfaff@users.noreply.github.com>
2023-09-17 08:37:55 +0000

    [MegaLinter] Apply linters fixes

--- pyrecest/filters/se2_ukf.py
+++ pyrecest/filters/se2_ukf.py
@@ -1,12 +1,13 @@
-import numpy as np
-from pyrecest.distributions import GaussianDistribution
-from .abstract_se2_filter import AbstractSE2Filter
 import copy
+
+import numpy as np
 from beartype import beartype
+from pyrecest.distributions import GaussianDistribution
+
+from .abstract_se2_filter import AbstractSE2Filter
 
 
 class SE2UKF(AbstractSE2Filter):
-
     @property
     def filter_state(self):
         return self._filter_state
@@ -15,6 +16,8 @@ class SE2UKF(AbstractSE2Filter):
     @beartype
     def filter_state(self, new_state: GaussianDistribution):
         assert np.size(new_state.mu) == 4
-        assert np.shape(new_state.C) == (4, 4), 'Wrong dimension.'
-        assert np.linalg.norm(new_state.mu[:2]) == 1, 'First two entries of estimate must be normalized'
+        assert np.shape(new_state.C) == (4, 4), "Wrong dimension."
+        assert (
+            np.linalg.norm(new_state.mu[:2]) == 1
+        ), "First two entries of estimate must be normalized"
         self._filter_state = copy.deepcopy(new_state)
