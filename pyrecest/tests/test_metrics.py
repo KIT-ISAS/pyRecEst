263d3195402b5bb311b9e9cf1823530d861f90c2
Florian Pfaff <pfaff@kit.edu>
2023-10-24 18:02:48 +0100

    More...

--- pyrecest/tests/test_metrics.py
+++ pyrecest/tests/test_metrics.py
@@ -3,7 +3,7 @@ import unittest
 import numpy.testing as npt
 
 # pylint: disable=no-name-in-module,no-member
-from pyrecest.backend import array, random, repeat, stack
+from pyrecest.backend import array, random, repeat, vstack, expand_dims
 from pyrecest.utils.metrics import anees
 
 
@@ -13,22 +13,27 @@ class TestANEES(unittest.TestCase):
         self.uncertainties = array(
             [[[1.0, 0.5], [0.5, 2.0]], [[1.0, 0.0], [0.0, 1.0]], [[0.5, 0.0], [0.0, 1.5]]]
         )
-        self.num_samples = 10000
+        self.n_timesteps_constant = 10000
 
     def test_ANEES_is_close_to_one(self):
+        """ Test that the ANEES is close to 1 when we sample from the groundtruths with the given uncertainties.
+        Simulate that the state stays constant for 10000 time steps, then changes, stays constant for another 10000 time steps
+        and then changes once more before staying constant for the remaining 10000 time steps.        
+        """
         samples = []
 
         for i in range(len(self.groundtruths)):
             samples_for_i = random.multivariate_normal(
                 mean=self.groundtruths[i],
                 cov=self.uncertainties[i],
-                size=self.num_samples,
+                size=self.n_timesteps_constant,
             )
             samples.append(samples_for_i)
 
-        samples_mat = stack(samples, axis=-1)
-        repeated_groundtruths = repeat(self.groundtruths, self.num_samples, axis=0)
-        repeated_uncertainties = repeat(self.uncertainties, self.num_samples, axis=0)
+        samples_mat = vstack(samples)
+        
+        repeated_groundtruths = repeat(self.groundtruths, repeats=self.n_timesteps_constant, axis=0)
+        repeated_uncertainties = repeat(self.uncertainties, repeats=self.n_timesteps_constant, axis=0)
 
         computed_ANEES = anees(samples_mat, repeated_uncertainties, repeated_groundtruths)
 
