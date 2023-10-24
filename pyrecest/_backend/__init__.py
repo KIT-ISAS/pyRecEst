7aead3136833b30045385f8eb4cc46dedf6045e4
Florian Pfaff <pfaff@kit.edu>
2023-10-24 16:32:17 +0100

    Default numpy

--- pyrecest/_backend/__init__.py
+++ pyrecest/_backend/__init__.py
@@ -13,9 +13,9 @@ import pyrecest._backend._common as common
 
 
 def get_backend_name():
-    # return os.environ.get("PYRECEST_BACKEND", "numpy")
+    return os.environ.get("PYRECEST_BACKEND", "numpy")
     #return os.environ.get("PYRECEST_BACKEND", "pytorch")
-    return os.environ.get("PYRECEST_BACKEND", "jax")
+    #return os.environ.get("PYRECEST_BACKEND", "jax")
 
 
 BACKEND_NAME = get_backend_name()
