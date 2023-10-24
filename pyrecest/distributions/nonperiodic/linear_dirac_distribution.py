fb88142193a9fc6f4f18a97561a7e0134283ae06
Florian Pfaff <pfaff@kit.edu>
2023-10-24 12:16:58 +0100

    Moore

--- pyrecest/distributions/nonperiodic/linear_dirac_distribution.py
+++ pyrecest/distributions/nonperiodic/linear_dirac_distribution.py
@@ -14,7 +14,7 @@ class LinearDiracDistribution(AbstractDiracDistribution, AbstractLinearDistribut
 
     def mean(self):
         # Like np.average(self.d, weights=self.w, axis=0) but for all backends
-        return (self.d * self.w.reshape(-1, 1)).sum(dim=0)
+        return self.w @ self.d
 
     def set_mean(self, new_mean):
         mean_offset = new_mean - self.mean
