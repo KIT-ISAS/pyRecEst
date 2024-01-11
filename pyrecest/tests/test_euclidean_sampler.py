2f5f562157626056abe2850bd24cb98c3a556c73
Florian Pfaff <pfaff@kit.edu>
2024-01-11 07:35:26 +0100

    Improved TestGaussianSampler

--- pyrecest/tests/test_euclidean_sampler.py
+++ pyrecest/tests/test_euclidean_sampler.py
@@ -1,11 +1,8 @@
 import unittest
-
 import numpy.testing as npt
-
-# pylint: disable=no-name-in-module,no-member
+from scipy.stats import shapiro
 from pyrecest.backend import mean, ones, random, std, zeros
 from pyrecest.sampling.euclidean_sampler import GaussianSampler
-
 
 class TestGaussianSampler(unittest.TestCase):
     def setUp(self):
@@ -15,19 +12,25 @@ class TestGaussianSampler(unittest.TestCase):
         self.dim = 2
         self.samples = self.sampler.sample_stochastic(self.n_samples, self.dim)
 
-    def test_sample_stochastic(self):
-        # Check that the returned shape matches the requested number of samples and dimension
+    def test_sample_stochastic_shape(self):
+        """Check that the returned shape matches the requested number of samples and dimension."""
         self.assertEqual(self.samples.shape, (self.n_samples, self.dim))
 
-    def test_gaussian_properties(self):
-        # Check that the mean is close to 0 for each dimension
+    def test_sample_mean_close_to_zero(self):
+        """Check that the mean is close to 0 for each dimension."""
         means = mean(self.samples, axis=0)
         npt.assert_allclose(means, zeros(self.dim), atol=0.15)
 
-        # Check that the standard deviation is close to 1 for each dimension
+    def test_sample_std_dev_close_to_one(self):
+        """Check that the standard deviation is close to 1 for each dimension."""
         std_devs = std(self.samples, axis=0)
         npt.assert_allclose(std_devs, ones(self.dim), atol=0.1)
 
+    def test_samples_follow_gaussian_distribution(self):
+        """Test if the samples follow a Gaussian distribution for each dimension."""
+        for i in range(self.dim):
+            _, p_value = shapiro(self.samples[:, i])
+            self.assertGreater(p_value, 0.05)
 
 if __name__ == "__main__":
     unittest.main()
