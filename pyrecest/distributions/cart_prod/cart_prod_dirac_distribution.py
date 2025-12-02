e7df20aa2241e776b14ba8d0b0f7c99d5690b8f7
FlorianPfaff <6773539+FlorianPfaff@users.noreply.github.com>
2025-12-02 20:11:56 +0000

    [MegaLinter] Apply linters automatic fixes

--- pyrecest/distributions/cart_prod/cart_prod_dirac_distribution.py
+++ pyrecest/distributions/cart_prod/cart_prod_dirac_distribution.py
@@ -1,5 +1,8 @@
-from .abstract_cart_prod_distribution import AbstractCartProdDistribution
 from ..abstract_dirac_distribution import AbstractDiracDistribution
+from .abstract_cart_prod_distribution import AbstractCartProdDistribution
 
-class CartProdDiracDistribution(AbstractDiracDistribution, AbstractCartProdDistribution):
+
+class CartProdDiracDistribution(
+    AbstractDiracDistribution, AbstractCartProdDistribution
+):
     pass
