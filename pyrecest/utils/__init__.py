1855ae1ac265df1a30a06a8054e4070fc35b4808
Florian Pfaff <FlorianPfaff@users.noreply.github.com>
2026-04-22 14:16:31 +0200

    Export Track2p convenience helpers for multisession assignment

--- pyrecest/utils/__init__.py
+++ pyrecest/utils/__init__.py
@@ -3,6 +3,8 @@
 from .multisession_assignment import (
     MultiSessionAssignmentResult,
     solve_multisession_assignment,
+    solve_multisession_assignment_from_similarity,
+    tracks_to_index_matrix,
     tracks_to_session_labels,
 )
 
@@ -16,6 +18,8 @@ from .nonrigid_point_set_registration import (
 __all__ = [
     "MultiSessionAssignmentResult",
     "solve_multisession_assignment",
+    "solve_multisession_assignment_from_similarity",
+    "tracks_to_index_matrix",
     "tracks_to_session_labels",
     "ThinPlateSplineRegistrationResult",
     "ThinPlateSplineTransform",
