2b85c5a2275768a3a13649faadaa906d131bf5ce
Florian Pfaff <FlorianPfaff@users.noreply.github.com>
2026-04-22 14:12:00 +0200

    Export weighted ROI similarity utilities

--- pyrecest/utils/__init__.py
+++ pyrecest/utils/__init__.py
@@ -6,10 +6,24 @@ from .nonrigid_point_set_registration import (
     estimate_thin_plate_spline,
     joint_tps_registration_assignment,
 )
+from .roi_similarity import (
+    build_roi_cost_matrix,
+    pairwise_centroid_distances,
+    pairwise_roi_similarity,
+    roi_centroid,
+    weighted_roi_cosine_similarity,
+    weighted_roi_jaccard,
+)
 
 __all__ = [
     "ThinPlateSplineRegistrationResult",
     "ThinPlateSplineTransform",
     "estimate_thin_plate_spline",
     "joint_tps_registration_assignment",
+    "build_roi_cost_matrix",
+    "pairwise_centroid_distances",
+    "pairwise_roi_similarity",
+    "roi_centroid",
+    "weighted_roi_cosine_similarity",
+    "weighted_roi_jaccard",
 ]
