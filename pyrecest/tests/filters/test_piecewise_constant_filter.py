6d4796885128ca346b3f663a6a9bf0cfae90786d
Florian Pfaff <FlorianPfaff@users.noreply.github.com>
2026-04-23 19:53:09 +0200

    Route copy imports through pyrecest compatibility module

--- pyrecest/tests/filters/test_piecewise_constant_filter.py
+++ pyrecest/tests/filters/test_piecewise_constant_filter.py
@@ -1,8 +1,8 @@
 import unittest
-from copy import copy
 
 import numpy.testing as npt
 import pyrecest.backend
+from pyrecest import copy
 
 # pylint: disable=no-name-in-module,no-member
 from pyrecest.backend import array, pi
@@ -63,7 +63,7 @@ class TestPiecewiseConstantFilter(unittest.TestCase):
 
         w = array([1.0, 2.0, 3.0, 4.0, 5.0, 5.0, 4.0, 3.0, 2.0, 1.0])
         self.f.filter_state = PiecewiseConstantDistribution(w)
-        w_before = copy(self.f.filter_state.w)
+        w_before = copy.copy(self.f.filter_state.w)
 
         A = array(np.eye(self.n))
         self.f.predict(A)
@@ -76,7 +76,7 @@ class TestPiecewiseConstantFilter(unittest.TestCase):
 
         w = array([1.0, 2.0, 3.0, 4.0, 5.0, 5.0, 4.0, 3.0, 2.0, 1.0])
         self.f.filter_state = PiecewiseConstantDistribution(w)
-        w_before = copy(self.f.filter_state.w)
+        w_before = copy.copy(self.f.filter_state.w)
 
         # Shift all weight to next interval (cyclic permutation)
         A = array(np.roll(np.eye(self.n), 1, axis=0))
@@ -100,7 +100,7 @@ class TestPiecewiseConstantFilter(unittest.TestCase):
         H = array(np.random.default_rng(0).uniform(0.5, 1.5, (lw, self.n)))
         z = 0.5  # falls in measurement interval 0 (first interval [0, 2pi/5))
 
-        w_before = copy(self.f.filter_state.w)
+        w_before = copy.copy(self.f.filter_state.w)
         self.f.update(H, z)
 
         # The updated weights should be proportional to H[0, :] * w_before
