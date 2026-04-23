f94ebdb20521f71233a59f7551cb14043b07a956
Florian Pfaff <FlorianPfaff@users.noreply.github.com>
2026-04-23 17:12:33 +0200

    Relax wrapped normal grid test tolerance

--- pyrecest/tests/distributions/test_circular_grid_distribution.py
+++ pyrecest/tests/distributions/test_circular_grid_distribution.py
@@ -36,13 +36,13 @@ class CircularGridDistributionTest(unittest.TestCase):
         mu = 0.8
         for sigma in arange(0.2, 2.1, 0.1):
             dist = WrappedNormalDistribution(mu, sigma)
-            self._test_grid_conversion(dist, 101, False, 1e-6)
+            self._test_grid_conversion(dist, 101, False, 3e-6)
 
     def test_WNToGridSqrt(self):
         mu = 0.9
         for sigma in arange(0.2, 2.1, 0.1):
             dist = WrappedNormalDistribution(mu, sigma)
-            self._test_grid_conversion(dist, 101, True, 1e-6)
+            self._test_grid_conversion(dist, 101, True, 3e-6)
 
 
 if __name__ == "__main__":
