750901acbc582d0c23d14125606ccbbe246a2aa3
Florian Pfaff <FlorianPfaff@users.noreply.github.com>
2026-04-23 21:51:12 +0200

    Fix wrapped normal convergence check

--- pyrecest/distributions/circle/wrapped_normal_distribution.py
+++ pyrecest/distributions/circle/wrapped_normal_distribution.py
@@ -83,7 +83,6 @@ class WrappedNormalDistribution(
             nc = 1.0 / sqrt(2.0 * pi) / self.sigma
 
             for i in range(n_inputs):
-                old_result = 0.0
                 result[i] = squeeze(exp(x[i] * x[i] * tmp))
 
                 for k in range(1, max_iterations + 1):
@@ -91,11 +90,13 @@ class WrappedNormalDistribution(
                     xm = x[i] - 2 * pi * k
                     tp = xp * xp * tmp
                     tm = xm * xm * tmp
-                    old_result = result[i]
-                    result[i] += (exp(tp) + exp(tm)).squeeze()
+                    increment = (exp(tp) + exp(tm)).squeeze()
+                    new_result = result[i] + increment
 
-                    if result[i] == old_result:
+                    if new_result == result[i]:
                         break
+
+                    result[i] = new_result
 
                 result[i] *= nc
         else:
