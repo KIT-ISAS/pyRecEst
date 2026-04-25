acd044437af4ab478642da0f42f28fbc608fdee0
Florian Pfaff <FlorianPfaff@users.noreply.github.com>
2026-04-25 11:18:41 +0200

    Add regression test for scalar filter parameters

---
+++ tests/test_evaluate_for_variables.py
@@ -0,0 +1,28 @@
+import unittest
+
+from pyrecest.evaluation.evaluate_for_variables import _expand_filter_configs
+
+
+class TestEvaluateForVariables(unittest.TestCase):
+    def test_expand_filter_configs_accepts_scalar_parameters(self):
+        filter_configs = [
+            {"name": "kf", "parameter": None},
+            {"name": "pf", "parameter": 100},
+            {"name": "pf", "parameter": [51, 81]},
+        ]
+
+        expanded_filter_configs = _expand_filter_configs(filter_configs)
+
+        self.assertEqual(
+            expanded_filter_configs,
+            [
+                {"name": "kf", "parameter": None},
+                {"name": "pf", "parameter": 100},
+                {"name": "pf", "parameter": 51},
+                {"name": "pf", "parameter": 81},
+            ],
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
