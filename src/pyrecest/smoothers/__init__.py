ab9a5d99287a7f8d2dd3e365c7ac01f86b046a02
Florian Pfaff <pfaff@ias.uni-stuttgart.de>
2026-04-28 13:34:59 +0200

    Add sliding-window manifold mean smoother

--- src/pyrecest/smoothers/__init__.py
+++ src/pyrecest/smoothers/__init__.py
@@ -1,5 +1,6 @@
 from .abstract_smoother import AbstractSmoother
 from .rauch_tung_striebel_smoother import RauchTungStriebelSmoother, RTSSmoother
+from .sliding_window_manifold_mean_smoother import SlidingWindowManifoldMeanSmoother
 from .unscented_rauch_tung_striebel_smoother import (
     UnscentedRauchTungStriebelSmoother,
     URTSSmoother,
@@ -9,6 +10,7 @@ __all__ = [
     "AbstractSmoother",
     "RauchTungStriebelSmoother",
     "RTSSmoother",
+    "SlidingWindowManifoldMeanSmoother",
     "UnscentedRauchTungStriebelSmoother",
     "URTSSmoother",
 ]
