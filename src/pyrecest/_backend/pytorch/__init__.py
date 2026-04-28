Fix biased covariance in pytorch backend

src/pyrecest/_backend/pytorch/__init__.py
@@ -175,22 +175,32 @@ def std(
 def cov(input, correction=1, fweights=None, aweights=None, bias=False):
     # for pyrecest
     if not bias:
-        return _torch.cov(input, correction=correction, fweights=fweights, aweights=aweights)
-    else:
-        assert fweights==None
-        # Ensure weights sum to 1
-        aweights = aweights / aweights.sum()
-        
-        # Calculate weighted means
-        means = (input * aweights).sum(dim=1, keepdim=True)
-        
-        deviation_centered = input - means
-        
-        # Calculate weighted biased covariance
-        cov_matrix = _torch.einsum('ij,kj,j->ik', deviation_centered, deviation_centered, aweights)
+        return _torch.cov(
+            input, correction=correction, fweights=fweights, aweights=aweights
+        )
+    assert fweights is None
 
-        return cov_matrix
-        
+    if aweights is None:
+        aweights = ones(input.shape[1], dtype=input.dtype, device=input.device)
+    else:
+        aweights = copy(asarray(aweights, dtype=input.dtype, device=input.device))
+
+    # Ensure weights sum to 1
+    aweights = aweights / sum(aweights)
+
+    # Calculate weighted means
+    means = sum(input * aweights, axis=1, keepdims=True)
+
+    deviation_centered = input - means
+
+    # Calculate weighted biased covariance
+    cov_matrix = _torch.einsum(
+        "ij,kj,j->ik", deviation_centered, deviation_centered, aweights
+    )
+
+    return cov_matrix
+
+
 def has_autodiff():
     """If allows for automatic differentiation.
 