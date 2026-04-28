833b6abe8c7f6c58a8a7ece0139ab8766a07a499
Florian Pfaff <pfaff@ias.uni-stuttgart.de>
2026-04-28 13:36:31 +0200

    Add SO3 chordal mean smoother

--- src/pyrecest/smoothers/__init__.py
+++ src/pyrecest/smoothers/__init__.py
@@ -1,5 +1,6 @@
 from .abstract_smoother import AbstractSmoother
 from .rauch_tung_striebel_smoother import RauchTungStriebelSmoother, RTSSmoother
+from .so3_chordal_mean_smoother import SO3ChordalMeanSmoother, SO3CMSmoother
 from .unscented_rauch_tung_striebel_smoother import (
     UnscentedRauchTungStriebelSmoother,
     URTSSmoother,
@@ -9,6 +10,8 @@ __all__ = [
     "AbstractSmoother",
     "RauchTungStriebelSmoother",
     "RTSSmoother",
+    "SO3ChordalMeanSmoother",
+    "SO3CMSmoother",
     "UnscentedRauchTungStriebelSmoother",
     "URTSSmoother",
 ]
