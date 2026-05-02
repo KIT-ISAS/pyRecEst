e8b99a84a6aebfe91cac510a7f3c3f8d87b8f6b2
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-02 03:37:53 +0200

    Add reusable particle model package

---
+++ src/pyrecest/models/__init__.py
@@ -0,0 +1,8 @@
+"""Reusable model objects for PyRecEst."""
+
+from .particle import LikelihoodMeasurementModel, SampleableTransitionModel
+
+__all__ = [
+    "LikelihoodMeasurementModel",
+    "SampleableTransitionModel",
+]
