52d1c1971ffee9b425d3c4721b7f01a07e4dd92c
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-02 03:37:29 +0200

    Export linear Gaussian model objects

--- src/pyrecest/models/__init__.py
+++ src/pyrecest/models/__init__.py
@@ -1,1 +1,13 @@
-"""Reusable state-space model objects."""
+"""Reusable model objects for recursive estimation."""
+
+from .linear_gaussian import IdentityGaussianMeasurementModel
+from .linear_gaussian import IdentityGaussianTransitionModel
+from .linear_gaussian import LinearGaussianMeasurementModel
+from .linear_gaussian import LinearGaussianTransitionModel
+
+__all__ = [
+    "IdentityGaussianMeasurementModel",
+    "IdentityGaussianTransitionModel",
+    "LinearGaussianMeasurementModel",
+    "LinearGaussianTransitionModel",
+]
