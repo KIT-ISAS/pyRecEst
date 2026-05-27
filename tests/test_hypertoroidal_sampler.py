c1f6662fa70fc3d3fd2f8ecaa5a951fde1a22075
Florian Pfaff <pfaff@ias.uni-stuttgart.de>
2026-05-27 23:29:00 +0200

    Validate circular sampler inputs

--- tests/test_hypertoroidal_sampler.py
+++ tests/test_hypertoroidal_sampler.py
@@ -1,5 +1,6 @@
 import unittest
 
+import numpy as np
 import numpy.testing as npt
 
 # pylint: disable=redefined-builtin,no-name-in-module,no-member
@@ -23,6 +24,24 @@ class TestCircularUniformSampler(unittest.TestCase):
         self.assertTrue(all(samples >= 0.0))
         self.assertTrue(all(samples < 2.0 * pi))
 
+    def test_sample_stochastic_accepts_integer_like_scalars(self):
+        samples = self.sampler.sample_stochastic(np.array(3.0), dim=np.array(1.0))
+
+        self.assertEqual(samples.shape, (3, 1))
+
+    def test_sample_stochastic_rejects_invalid_controls(self):
+        invalid_sample_counts = (-1, 1.5, True, [2])
+        for n_samples in invalid_sample_counts:
+            with self.subTest(n_samples=n_samples):
+                with self.assertRaises(ValueError):
+                    self.sampler.sample_stochastic(n_samples)
+
+        invalid_dims = (0, 1.5, 2, True, [1])
+        for dim in invalid_dims:
+            with self.subTest(dim=dim):
+                with self.assertRaises(ValueError):
+                    self.sampler.sample_stochastic(2, dim=dim)
+
     def test_get_grid(self):
         grid_density_parameter = 100
         grid_points = self.sampler.get_grid(grid_density_parameter)
@@ -42,6 +61,17 @@ class TestCircularUniformSampler(unittest.TestCase):
             self.sampler.get_grid(0)
         with self.assertRaises(ValueError):
             self.sampler.get_grid(-1)
+
+    def test_get_grid_accepts_integer_like_scalar_density(self):
+        grid_points = self.sampler.get_grid(np.array(4.0))
+
+        self.assertEqual(grid_points.shape[0], 4)
+
+    def test_get_grid_rejects_noninteger_density(self):
+        for density in (2.5, True, [3]):
+            with self.subTest(density=density):
+                with self.assertRaises(ValueError):
+                    self.sampler.get_grid(density)
 
 
 if __name__ == "__main__":
