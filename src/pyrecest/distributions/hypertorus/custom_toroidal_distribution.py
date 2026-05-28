4703ffff8f2564ecead103666553c1cdfdb55dc0
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-28 22:16:50 +0200

    Allow custom toroidal shift and scale parameters

--- src/pyrecest/distributions/hypertorus/custom_toroidal_distribution.py
+++ src/pyrecest/distributions/hypertorus/custom_toroidal_distribution.py
@@ -5,6 +5,8 @@ from .custom_hypertoroidal_distribution import CustomHypertoroidalDistribution
 class CustomToroidalDistribution(
     CustomHypertoroidalDistribution, AbstractToroidalDistribution
 ):
-    def __init__(self, f):
+    def __init__(self, f, scale_by=1, shift_by=None):
         AbstractToroidalDistribution.__init__(self)
-        CustomHypertoroidalDistribution.__init__(self, f, 2)
+        CustomHypertoroidalDistribution.__init__(
+            self, f, 2, shift_by=shift_by, scale_by=scale_by
+        )
