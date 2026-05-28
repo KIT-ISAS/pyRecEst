6b5ff6270bb3e433eaf3ef5bee0660573caadffb
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-29 01:10:59 +0200

    Preserve custom hypertoroidal scale and shift on conversion (#2406)
    * Preserve custom hypertoroidal scale and shift on conversion
    
    * Allow custom toroidal shift and scale parameters
    
    * Cover custom hypertoroidal conversion regressions

--- src/pyrecest/distributions/hypertorus/custom_hypertoroidal_distribution.py
+++ src/pyrecest/distributions/hypertorus/custom_hypertoroidal_distribution.py
@@ -10,7 +10,7 @@ from .abstract_hypertoroidal_distribution import AbstractHypertoroidalDistributi
 class CustomHypertoroidalDistribution(
     AbstractHypertoroidalDistribution, AbstractCustomDistribution
 ):
-    def __init__(self, f, dim, shift_by=None):
+    def __init__(self, f, dim, shift_by=None, scale_by=1):
         # Constructor, it is the user's responsibility to ensure that f is a valid
         # hypertoroidal density and takes arguments of the same form as
         # .pdf, i.e., it needs to be vectorized.
@@ -20,7 +20,7 @@ class CustomHypertoroidalDistribution(
         #       pdf of the distribution
         #   dim (scalar)
         #       dimension of the hypertorus
-        AbstractCustomDistribution.__init__(self, f)
+        AbstractCustomDistribution.__init__(self, f, scale_by)
         AbstractHypertoroidalDistribution.__init__(self, dim)
         if shift_by is None:
             self.shift_by = zeros(dim)
@@ -38,7 +38,9 @@ class CustomHypertoroidalDistribution(
         #   ccd (CustomCircularDistribution)
         #       CustomCircularDistribution with same parameters
         assert self.dim == 1
-        ccd = CustomCircularDistribution(self.f)
+        ccd = CustomCircularDistribution(
+            self.f, scale_by=self.scale_by, shift_by=self.shift_by[0]
+        )
         return ccd
 
     def to_custom_toroidal(self):
@@ -50,5 +52,7 @@ class CustomHypertoroidalDistribution(
         from .custom_toroidal_distribution import CustomToroidalDistribution
 
         assert self.dim == 2
-        ctd = CustomToroidalDistribution(self.f)
+        ctd = CustomToroidalDistribution(
+            self.f, scale_by=self.scale_by, shift_by=self.shift_by
+        )
         return ctd
