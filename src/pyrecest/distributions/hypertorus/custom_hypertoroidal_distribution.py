883ad920e0a5911e026783bec2b38b494e190266
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-28 18:45:44 +0200

    Fix custom hypertoroidal array-like handling

--- src/pyrecest/distributions/hypertorus/custom_hypertoroidal_distribution.py
+++ src/pyrecest/distributions/hypertorus/custom_hypertoroidal_distribution.py
@@ -1,5 +1,5 @@
 # pylint: disable=no-name-in-module,no-member
-from pyrecest.backend import mod, pi, zeros
+from pyrecest.backend import asarray, mod, pi, reshape, zeros
 
 from ..abstract_custom_distribution import AbstractCustomDistribution
 from ..circle.custom_circular_distribution import CustomCircularDistribution
@@ -9,7 +9,7 @@ from .abstract_hypertoroidal_distribution import AbstractHypertoroidalDistributi
 class CustomHypertoroidalDistribution(
     AbstractHypertoroidalDistribution, AbstractCustomDistribution
 ):
-    def __init__(self, f, dim, shift_by=None):
+    def __init__(self, f, dim, shift_by=None, scale_by=1):
         # Constructor, it is the user's responsibility to ensure that f is a valid
         # hypertoroidal density and takes arguments of the same form as
         # .pdf, i.e., it needs to be vectorized.
@@ -19,14 +19,22 @@ class CustomHypertoroidalDistribution(
         #       pdf of the distribution
         #   dim (scalar)
         #       dimension of the hypertorus
-        AbstractCustomDistribution.__init__(self, f)
+        AbstractCustomDistribution.__init__(self, f, scale_by)
         AbstractHypertoroidalDistribution.__init__(self, dim)
         if shift_by is None:
             self.shift_by = zeros(dim)
         else:
+            shift_by = asarray(shift_by)
+            if dim == 1 and shift_by.ndim == 0:
+                shift_by = reshape(shift_by, (1,))
+            if shift_by.shape != (dim,):
+                raise ValueError(
+                    "shift_by must be a vector with one entry per hypertoroidal dimension"
+                )
             self.shift_by = shift_by
 
     def pdf(self, xs):
+        xs = asarray(xs)
         return AbstractCustomDistribution.pdf(self, mod(xs + self.shift_by, 2 * pi))
 
     def to_custom_circular(self):
@@ -36,7 +44,9 @@ class CustomHypertoroidalDistribution(
         #   ccd (CustomCircularDistribution)
         #       CustomCircularDistribution with same parameters
         assert self.dim == 1
-        ccd = CustomCircularDistribution(self.f)
+        ccd = CustomCircularDistribution(
+            self.f, scale_by=self.scale_by, shift_by=self.shift_by[0]
+        )
         return ccd
 
     def to_custom_toroidal(self):
@@ -48,5 +58,7 @@ class CustomHypertoroidalDistribution(
         from .custom_toroidal_distribution import CustomToroidalDistribution
 
         assert self.dim == 2
-        ctd = CustomToroidalDistribution(self.f)
+        ctd = CustomToroidalDistribution(
+            self.f, scale_by=self.scale_by, shift_by=self.shift_by
+        )
         return ctd
