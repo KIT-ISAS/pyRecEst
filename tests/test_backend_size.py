567dbd1dee40f61fa694e32702adad59e3cf4111
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-03 07:00:58 +0200

    Add backend size and comb regression tests

--- tests/test_backend_size.py
+++ tests/test_backend_size.py
@@ -1,6 +1,6 @@
 import unittest
 
-from pyrecest.backend import array, size
+from pyrecest.backend import array, comb, size
 
 
 class TestBackendSize(unittest.TestCase):
@@ -9,6 +9,22 @@ class TestBackendSize(unittest.TestCase):
 
     def test_size_returns_axis_length(self):
         self.assertEqual(size(array([[1, 2, 3], [4, 5, 6]]), axis=1), 3)
+
+    def test_size_handles_python_array_like_inputs(self):
+        values = [[1, 2, 3], [4, 5, 6]]
+
+        self.assertEqual(size(values), 6)
+        self.assertEqual(size(values, axis=0), 2)
+        self.assertEqual(size(values, axis=1), 3)
+
+    def test_size_handles_python_scalars(self):
+        self.assertEqual(size(3.0), 1)
+
+
+class TestBackendComb(unittest.TestCase):
+    def test_comb_returns_standard_binomial_values(self):
+        self.assertEqual(comb(5, 2), 10)
+        self.assertEqual(comb(3, 4), 0)
 
 
 if __name__ == "__main__":
