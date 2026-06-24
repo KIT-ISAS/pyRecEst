fcf59530eaeb2cec26ba495d08812eac1f0e4d68
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-24 13:05:10 +0200

    Update placeholder import

--- tests/test_track_completion_text_candidate_validation.py
+++ tests/test_track_completion_text_candidate_validation.py
@@ -1,2 +1,5 @@
+from pyrecest.utils.track_completion import enumerate_fragment_completion_paths
+
+
 def test_placeholder():
-    assert True
+    assert enumerate_fragment_completion_paths is not None
