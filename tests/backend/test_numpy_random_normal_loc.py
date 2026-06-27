f1a7d4b5e3e9cea7734da9d34bcaa0a51800f762
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-28 01:21:19 +0200

    Validate NumPy normal location parameters (#3243)
    * Validate NumPy normal location parameter
    
    * Add NumPy normal location validation test

---
+++ tests/backend/test_numpy_random_normal_loc.py
@@ -0,0 +1,8 @@
+import pytest
+
+from pyrecest._backend.numpy import random
+
+
+def test_normal_rejects_boolean_location_parameter():
+    with pytest.raises(TypeError):
+        random.normal(loc=True)
