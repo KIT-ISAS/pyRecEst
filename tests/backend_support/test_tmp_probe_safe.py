6515b31a5838f5b53f36aae7a915641792e376c4
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-29 14:55:33 +0200

    Update temporary safe probe

--- tests/backend_support/test_tmp_probe_safe.py
+++ tests/backend_support/test_tmp_probe_safe.py
@@ -1,5 +1,6 @@
 # simple
-from math import prod as _prod
+import numpy as _np
+import torch as _torch
 
 def test_tmp_probe_safe():
-    assert _prod([1, 2, 3]) == 6
+    assert _torch.as_tensor(_np.asarray([1])).tolist() == [1]
