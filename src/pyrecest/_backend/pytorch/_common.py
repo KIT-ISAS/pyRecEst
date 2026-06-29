d3d582b212f4a9a1d9a4d971875781d17e523d51
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-29 13:22:20 +0200

    Accept torch dtype string aliases in PyTorch common helpers

--- src/pyrecest/_backend/pytorch/_common.py
+++ src/pyrecest/_backend/pytorch/_common.py
@@ -1,25 +1,44 @@
 import numpy as _np
-import torch as _torch
+from torch import (
+    bool as _torch_bool,
+    complex64 as _torch_complex64,
+    complex128 as _torch_complex128,
+    dtype as _torch_dtype,
+    float16 as _torch_float16,
+    float32 as _torch_float32,
+    float64 as _torch_float64,
+    from_numpy as _torch_from_numpy,
+    int8 as _torch_int8,
+    int16 as _torch_int16,
+    int32 as _torch_int32,
+    int64 as _torch_int64,
+    is_tensor as _torch_is_tensor,
+    stack as _torch_stack,
+    tensor as _torch_tensor,
+    uint8 as _torch_uint8,
+)
 
 _TORCH_DTYPE_BY_NAME = {
-    "bool": _torch.bool,
-    "uint8": _torch.uint8,
-    "int8": _torch.int8,
-    "int16": _torch.int16,
-    "int32": _torch.int32,
-    "int64": _torch.int64,
-    "float16": _torch.float16,
-    "float32": _torch.float32,
-    "float64": _torch.float64,
-    "complex64": _torch.complex64,
-    "complex128": _torch.complex128,
+    "bool": _torch_bool,
+    "uint8": _torch_uint8,
+    "int8": _torch_int8,
+    "int16": _torch_int16,
+    "int32": _torch_int32,
+    "int64": _torch_int64,
+    "float16": _torch_float16,
+    "float32": _torch_float32,
+    "float64": _torch_float64,
+    "complex64": _torch_complex64,
+    "complex128": _torch_complex128,
 }
 
 
 def _normalize_dtype(dtype):
-    """Return a torch dtype for dtype-like values understood by NumPy."""
-    if dtype is None or isinstance(dtype, _torch.dtype):
+    """Return a torch dtype for dtype-like values understood by PyTorch/NumPy."""
+    if dtype is None or isinstance(dtype, _torch_dtype):
         return dtype
+    if isinstance(dtype, str) and dtype.startswith("torch."):
+        dtype = dtype.split(".", 1)[1]
     try:
         return _TORCH_DTYPE_BY_NAME[str(_np.dtype(dtype))]
     except (KeyError, TypeError):
@@ -27,16 +46,16 @@ def _normalize_dtype(dtype):
 
 
 def from_numpy(x):
-    if _torch.is_tensor(x):
+    if _torch_is_tensor(x):
         return x
     if isinstance(x, _np.ndarray) and any(stride < 0 for stride in x.strides):
         x = x.copy()
-    return _torch.from_numpy(x)
+    return _torch_from_numpy(x)
 
 
 def array(val, dtype=None):
     dtype = _normalize_dtype(dtype)
-    if _torch.is_tensor(val):
+    if _torch_is_tensor(val):
         if dtype is None or val.dtype == dtype:
             return val.clone()
 
@@ -51,13 +70,13 @@ def array(val, dtype=None):
 
     if isinstance(val, (list, tuple)) and len(val):
         tensors = [array(tensor, dtype=dtype) for tensor in val]
-        return _torch.stack(tensors)
+        return _torch_stack(tensors)
 
-    return _torch.tensor(val, dtype=dtype)
+    return _torch_tensor(val, dtype=dtype)
 
 
 def cast(x, dtype):
     dtype = _normalize_dtype(dtype)
-    if _torch.is_tensor(x):
+    if _torch_is_tensor(x):
         return x.to(dtype=dtype)
     return array(x, dtype=dtype)
