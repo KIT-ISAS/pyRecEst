7bec97bdc15cd78d2c218bec7305b4c067c52975
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-29 16:13:21 +0200

    Tidy

--- src/pyrecest/_backend/pytorch_copy_contract_candidate.py
+++
@@ -1,9 +0,0 @@
-def patch_backend_copy(backend):
-    original_copy = backend.copy
-
-    def copy_arraylike(x):
-        if backend.is_array(x):
-            return original_copy(x)
-        return backend.array(x)
-
-    backend.copy = copy_arraylike
