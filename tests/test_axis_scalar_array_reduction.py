32bc127f0d8c6b65c12f22135b9a40f4de8a915a
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-30 21:39:34 +0200

    Remove scalar array reduction scratch test

--- tests/test_axis_scalar_array_reduction.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-from pyrecest._backend import _common
-
-
-def test_normalize_reduction_axes_accepts_scalar_array_axis():
-    assert _common._normalize_reduction_axes(np.asarray(0), 2) == (0,)
