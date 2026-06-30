c8bc046290391bd75c2acaa08942b4ec62954710
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-30 21:42:30 +0200

    Add reduction axis regression

---
+++ tests/test_common_reduction_axis_scalar.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+from pyrecest._backend import _common
+
+
+def test_numpy_scalar_integer_reduction_axis():
+    assert _common._normalize_reduction_axes(np.asarray(0), 2) == (0,)
