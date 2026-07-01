cbb71b4649cfc6fd4e206cedfc2e852f6faae773
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-01 20:54:37 +0200

    Probe lambda content

--- src/pyrecest/filters/wrapped_normal_filter.py
+++ src/pyrecest/filters/wrapped_normal_filter.py
@@ -1,11 +1,1 @@
-import math
-from collections.abc import Callable
-
-# pylint: disable=redefined-builtin,no-name-in-module,no-member
-from pyrecest.backend import array, log, max, maximum, min, minimum, mod, pi
-from pyrecest.distributions import CircularDiracDistribution, WrappedNormalDistribution
-
-from .abstract_filter import AbstractFilter
-from .manifold_mixins import CircularFilterMixin
-
-_PROGRESSIVE_TAU_MESSAGE = "tau must be a positive finite scalar"
+x = lambda points: points
